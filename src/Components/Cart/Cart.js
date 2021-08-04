0099f2805593f58610dbdc6df407e11bf50e6c04
mdemon420 <mdeamon4@gmail.com>
2021-08-04 13:30:45 +0600

    complete

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const cart = (props) => {
+    const cart = props.cart;
+
+    const totalPrice = cart.reduce((total,product) => total+product.price,0);
+
+    let shipping = 0;
+    if(totalPrice > 35){
+        shipping = 0;
+    }
+    else if(totalPrice >15){
+        shipping = 5;
+    }
+    else if(totalPrice >0){
+        shipping = 12;
+    }
+    return (
+        <div>
+            <h2>Summay </h2>
+            <p>Item Ordered: {cart.length}</p>
+            <p>Product price: {totalPrice}</p>
+            <p>Shipping Cost: {shipping}</p>
+            <p>Total Price: {totalPrice + shipping}</p>
+        </div>
+    );
+};
+
+export default cart;
