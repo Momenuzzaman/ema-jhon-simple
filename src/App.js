8bf20c0e4e0a4bea20686f8fde12362776830719
mdemon420 <mdeamon4@gmail.com>
2021-08-14 16:27:56 +0600

    router add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Shop from './Components/Shop/Shop';
+import Review from './Components/Review/Review';
+import Inventory from './Components/Inventory/Inventory';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-     <Header></Header>
-     <Shop></Shop>
+      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+             <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+          <Route path="/manage">
+            <Inventory></Inventory>
+          </Route>
+        </Switch>
+      </Router>
+     
+     
     </div>
   )
 }
