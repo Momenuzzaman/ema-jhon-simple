8190493b4826738c8521350e8ba95f5a82ed84ef
mdemon420 <mdeamon4@gmail.com>
2021-08-19 00:10:11 +0600

    review add

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+
+   const [cart,setCart]=useState([]);
+
+   useEffect(() => {
+       const saveCart = getDatabaseCart();
+       const productKey = Object.keys(saveCart);
+       const counts = productKey.map(key =>saveCart[key]);
+       console.log(counts);
+   })
     return (
         <div>
             <h1> This is Review</h1>
